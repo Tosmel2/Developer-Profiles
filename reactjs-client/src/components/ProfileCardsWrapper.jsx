ceab037dc15a4ce9392f7fc55c3e83db2da05985
tosmel2 <adewaletosin0808@gmail.com>
2023-08-26 04:27:31 -0700

    refactor: updated the profile card wrapper components

--- reactjs-client/src/components/ProfileCardsWrapper.jsx
+++ reactjs-client/src/components/ProfileCardsWrapper.jsx
@@ -17,7 +17,7 @@ function ProfileCardsWrapper({
     );
   }
 
-  if (profilesData && profilesData.profiles.length < 1) {
+  if (profilesData && profilesData.allProfiles.length < 1) {
     return (
       <p className='my-40 text-center text-gray-400 text-[20px] mx-auto'>
         No profiles added yet!!! <br /> Please add add one or more profiles to
@@ -26,13 +26,13 @@ function ProfileCardsWrapper({
     );
   }
 
-  if (profilesData && profilesData.profiles.length > 0) {
+  if (profilesData && profilesData.allProfiles.length > 0) {
     return (
       <section
         className='profile-cards-wrapper mt-8 sm:mt-20 flex flex-col gap-6 md:mx-auto md:gap-y-12 justify-center md:gap-x-16 md:flex md:flex-wrap md:flex-row'
         id='profileCardsWrapper'
       >
-        {profilesData.profiles.map((profile) => {
+        {profilesData.allProfiles.map((profile) => {
           const { _id } = profile;
 
           return (
